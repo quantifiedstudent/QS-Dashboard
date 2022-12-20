9ac4dfd70d71f25de1027f0077584bcf182d6e70
Stijn Verhagen <stijn@moonlysoftware.com>
2022-12-20 11:52:27 +0100

    Full refactor to Tailwind

--- src/components/dashboard/menu/Menu.tsx
+++ src/components/dashboard/menu/Menu.tsx
@@ -11,79 +11,77 @@ import graphEditorIcon from "@Assets/icons/graph_editor_icon.svg";
 import dataSharingIcon from "@Assets/icons/data_sharing.svg";
 import notificationsIcon from "@Assets/icons/notifications.svg";
 
-import styles from "./style.module.scss";
-
 const Menu = () => {
-  const [GraphModal, ShowGraphModal] = useState(false);
+    const [GraphModal, ShowGraphModal] = useState(false);
 
-  return (
-    <>
-      <GraphEditor
-        handleClose={() => ShowGraphModal(false)}
-        show={GraphModal}
-      />
-      <div className={styles.menu_container}>
-        <div>
-          <h3>Hello, Thijmen!</h3>
-          <p>you have 3 new notifications</p>
-        </div>
-        <div className={styles.button_container}>
-          <div>
-            <BaseButton>
-              <>
-                <Image
-                  src={goalsIcon}
-                  width={27.14}
-                  height={27.14}
-                  alt="Goals icon"
-                />
-                <Text>goals</Text>
-              </>
-            </BaseButton>
-          </div>
-          <div>
-            <BaseButton onPress={() => ShowGraphModal(true)}>
-              <>
-                <Image
-                  src={graphEditorIcon}
-                  width={27.14}
-                  height={27.14}
-                  alt="Graph editor icon"
-                />
-                <Text>Graph editor</Text>
-              </>
-            </BaseButton>
-          </div>
-          <div>
-            <BaseButton>
-              <>
-                <Image
-                  src={dataSharingIcon}
-                  width={27.14}
-                  height={27.14}
-                  alt="datasharing icon"
-                />
-                <Text>data sharing</Text>
-              </>
-            </BaseButton>
-          </div>
-          <div>
-            <BaseButton>
-              <>
-                <Image
-                  src={notificationsIcon}
-                  width={27.14}
-                  height={27.14}
-                  alt="notifications icon"
-                />
-                <Text>notifications</Text>
-              </>
-            </BaseButton>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <GraphEditor
+                handleClose={() => ShowGraphModal(false)}
+                show={GraphModal}
+            />
+            <div className="box-border flex items-center justify-between p-4">
+                <div>
+                    <h3>Hello, Thijmen!</h3>
+                    <p>You have 3 new notifications</p>
+                </div>
+                <div className="flex justify-between w-7/12">
+                    <div>
+                        <BaseButton>
+                            <>
+                                <Image
+                                    src={goalsIcon}
+                                    width={27.14}
+                                    height={27.14}
+                                    alt="Goals icon"
+                                />
+                                <Text>Goals</Text>
+                            </>
+                        </BaseButton>
+                    </div>
+                    <div>
+                        <BaseButton onPress={() => ShowGraphModal(true)}>
+                            <>
+                                <Image
+                                    src={graphEditorIcon}
+                                    width={27.14}
+                                    height={27.14}
+                                    alt="Graph editor icon"
+                                />
+                                <Text>Graph editor</Text>
+                            </>
+                        </BaseButton>
+                    </div>
+                    <div>
+                        <BaseButton>
+                            <>
+                                <Image
+                                    src={dataSharingIcon}
+                                    width={27.14}
+                                    height={27.14}
+                                    alt="datasharing icon"
+                                />
+                                <Text>Data sharing</Text>
+                            </>
+                        </BaseButton>
+                    </div>
+                    <div>
+                        <BaseButton>
+                            <>
+                                <Image
+                                    src={notificationsIcon}
+                                    width={27.14}
+                                    height={27.14}
+                                    alt="notifications icon"
+                                />
+                                <Text>Notifications</Text>
+                            </>
+                        </BaseButton>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Menu;
--- src/components/misc/card/baseCard.tsx
+++ src/components/misc/card/baseCard.tsx
@@ -1,28 +1,28 @@
 import { Card } from "@nextui-org/react";
 
 interface BaseButtonProps {
-  onPress?: () => void;
-  children: JSX.Element;
+    onPress?: () => void;
+    children: JSX.Element;
 }
 
 const BaseCard = (props: BaseButtonProps) => {
-  return (
-    <Card
-      onPress={props.onPress}
-      css={{
-        backgroundColor: "#FCFCFC",
-        border: "1px solid #DBDBDB",
-        boxShadow: "0px 2px 3px #0000000D",
-        filter: "none",
-        padding: "$6",
-        minWidth: "300px",
-        maxWidth: "300px",
-        marginRight: "$10",
-      }}
-    >
-      {props.children}
-    </Card>
-  );
+    return (
+        <Card
+            onPress={props.onPress}
+            css={{
+                backgroundColor: "#FCFCFC",
+                border: "1px solid #DBDBDB",
+                boxShadow: "0px 2px 3px #0000000D",
+                filter: "none",
+                padding: "$6",
+                minWidth: "300px",
+                maxWidth: "300px",
+                marginRight: "$10",
+            }}
+        >
+            {props.children}
+        </Card>
+    );
 };
 
 export default BaseCard;
